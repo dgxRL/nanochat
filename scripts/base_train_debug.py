66ba27724af1aa456773b5a4c269720721db8b32
dongSheng <dongsheng@mini64A.local>
2026-01-26 19:33:47 -0800

    reduce eval-tokens

--- scripts/base_train_debug.py
+++ scripts/base_train_debug.py
@@ -63,7 +63,7 @@ parser.add_argument("--final-lr-frac", type=float, default=0.0, help="final LR a
 parser.add_argument("--resume-from-step", type=int, default=-1, help="resume training from this step (-1 = disable)")
 # Evaluation
 parser.add_argument("--eval-every", type=int, default=250, help="evaluate val bpb every N steps (-1 = disable)")
-parser.add_argument("--eval-tokens", type=int, default=20*524288, help="number of tokens to evaluate val loss on")
+parser.add_argument("--eval-tokens", type=int, default=20*4098, help="number of tokens to evaluate val loss on")
 parser.add_argument("--core-metric-every", type=int, default=100, help="evaluate CORE metric every N steps (-1 = disable)")
 parser.add_argument("--core-metric-max-per-task", type=int, default=500, help="examples per task for CORE metric")
 parser.add_argument("--sample-every", type=int, default=100, help="sample from model every N steps (-1 = disable)")
